f884f6d34c205cc24ab085b1eb6f374f272baae5
이윤환 <zoo7930@sju.ac.kr>
2022-01-01 15:56:05 +0900

    Pattern 객체 필드에 static final로 선언

--- programmers/level1/신규 아이디 추천_정규식사용.java
+++ programmers/level1/신규 아이디 추천_정규식사용.java
@@ -1,5 +1,13 @@
+import java.util.regex.Pattern;
+
 class Solution {
-    
+
+    private static final Pattern letterRegex = Pattern.compile("[^a-zA-Z0-9_.-]");
+    private static final Pattern DotsRegex = Pattern.compile("[.]{2,}");
+    private static final Pattern frontDotRegex = Pattern.compile("^[.]");
+    private static final Pattern backDotRegex = Pattern.compile("[.]$");
+    private static final Pattern bothEndsDotsRegex = Pattern.compile("^[.]|[.]$");
+
     public String solution(String new_id) {
 
         return new RecommendationID(new_id)
@@ -31,22 +39,29 @@ class Solution {
         }
 
         private RecommendationID removeUnavailableLetter(){
-            this.id = id.replaceAll("[^a-zA-Z0-9_.-]","");
+//            this.id = id.replaceAll("[^a-zA-Z0-9_.-]","");
+            this.id = letterRegex.matcher(this.id).replaceAll("");
             return this;
         }
 
         private RecommendationID changeDotsIntoOneDot(){
-            this.id = id.replaceAll("[.]{2,}",".");
+//            this.id = id.replaceAll("[.]{2,}",".");
+            this.id = DotsRegex.matcher(this.id).replaceAll(".");
+
             return this;
         }
 
         private RecommendationID removeFrontDot(){
-            this.id = id.replaceAll("^[.]","");
+//            this.id = id.replaceAll("^[.]","");
+            this.id = frontDotRegex.matcher(this.id).replaceAll("");
+
             return this;
         }
 
         private RecommendationID removeBackDot(){
-            this.id = id.replaceAll("[.]$","");
+//            this.id = id.replaceAll("[.]$","");
+            this.id = backDotRegex.matcher(this.id).replaceAll("");
+
             return this;
         }
 
@@ -56,7 +71,11 @@ class Solution {
         }
 
         private RecommendationID removeIfMoreThan15(){
-            if(this.id.length() >= 16) this.id = this.id.substring(0,15).replaceAll("^[.]|[.]$","");
+            if(this.id.length() >= 16) {
+//                this.id = this.id.substring(0,15).replaceAll("^[.]|[.]$","");
+                this.id = bothEndsDotsRegex.matcher(this.id.substring(0,15)).replaceAll("");
+
+            }
             return this;
         }
 
