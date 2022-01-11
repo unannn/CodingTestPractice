617ef775620fd30267025a9742b6639a7cfa1420
이윤환 <zoo7930@sju.ac.kr>
2022-01-11 12:58:41 +0900

    2022/1/11 - 부족한 금액 계산하기

---
+++ programmers/level1/부족한 금액 계산하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long solution(long price, int money, int count) {
+        long sum = 0;
+        for(int i = 1;i <= count;i++){
+            sum += price * i;
+        }
+        return money - sum < 0 ? sum - money : 0;
+    }
+}
