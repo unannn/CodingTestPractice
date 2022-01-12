0633681816f6b10ce76d3710f69b112bab423f4d
이윤환 <zoo7930@sju.ac.kr>
2022-01-13 01:25:01 +0900

    2022/1/12 - 최대공약수와 최소공배수

---
+++ programmers/level1/최대공약수와 최소공배수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int gcd = gcd(n, m);
+        return new int[]{gcd, n * m / gcd};
+    }
+
+    private int gcd(int a, int b){
+        if (b == 0) {
+            return a;
+        }
+        else{
+            return gcd(b, a % b);
+        }
+    }
+}
