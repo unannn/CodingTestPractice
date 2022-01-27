fdf2c2afab887d7d11764ca298ee6e0aafc24580
이윤환 <zoo7930@sju.ac.kr>
2022-01-27 23:30:41 +0900

    2022/1/27 - 아스키 코드

---
+++ baekjoon/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        System.out.println((int)next.charAt(0));
+    }
+}
