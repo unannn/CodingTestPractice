82b56961c3b9939e120b77efea6fda7ebf1ed31b
이윤환 <zoo7930@sju.ac.kr>
2022-01-30 11:57:54 +0900

    2022/1/30 - [1차] 비밀지도 - 비트 연산자 | 사용

--- programmers/level1/[1차] 비밀지도.java
+++ programmers/level1/[1차] 비밀지도.java
@@ -1,39 +1,19 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        StringBuilder sb;
-        ArrayList<String> Strings1 = new ArrayList<>();
-        ArrayList<String> Strings2 = new ArrayList<>();
-        setBinaryStrings(Strings1, arr1, n);
-        setBinaryStrings(Strings2, arr2, n);
 
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
-            sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if(Strings1.get(i).charAt(j) == '1' || Strings2.get(i).charAt(j) == '1'){
-                    sb.append("#");
-                }
-                else{
-                    sb.append(" ");
-                }
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
+            StringBuilder sb = new StringBuilder(s);
+
+            while(sb.length() < n) {
+                sb.insert(0, '0');
             }
-            answer[i] = sb.toString();
+            answer[i] = sb.toString()
+                    .replaceAll("1","#")
+                    .replaceAll("0"," ");
         }
 
         return answer;
-    }
-
-    private void setBinaryStrings(ArrayList<String> Strings, int[] arr,int n) {
-        StringBuilder sb;
-        for (int i : arr) {
-            sb = new StringBuilder();
-            sb.append(String.format(Integer.toBinaryString(i)));
-            while(sb.length() < n){
-                sb.insert(0, "0");
-            }
-            Strings.add(sb.toString());
-        }
     }
 }
