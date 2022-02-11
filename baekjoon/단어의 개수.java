1fa1e175fde390c72906755ff60a620a96f41e7e
이윤환 <zoo7930@sju.ac.kr>
2022-02-11 12:43:18 +0900

    2022/2/11 - 단어의 개수

---
+++ baekjoon/단어의 개수.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(s);
+        System.out.println(st.countTokens());
+    }
+}
