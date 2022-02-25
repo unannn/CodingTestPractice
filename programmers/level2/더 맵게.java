b2a6832423fd94e6f17a010bd6a6036db2f22281
이윤환 <zoo7930@sju.ac.kr>
2022-02-25 14:31:23 +0900

    2022/2/25 - 더 맵게 - 변수 수정

--- programmers/level2/더 맵게.java
+++ programmers/level2/더 맵게.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
         Queue<Integer> heap = new PriorityQueue<>();
         for (Integer s : scoville) {
             heap.add(s);
         }
 
+        int answer = 0;
         Integer first = null, second = null;
         while ((first = heap.poll()) != null
-                && (second = heap.poll()) != null){ // &&문은 왼쪽부터 계산
+                && (second = heap.poll()) != null){ // &&문은 왼쪽부터 계산, 먼저 계산한 값이 거짓이면 더이상 연산 X
             heap.add(first + second * 2);
             answer++;
             if(heap.peek() >= K){
