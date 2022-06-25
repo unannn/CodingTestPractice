445ba9fdc2a283d94c0a0fa9344135b637e186b1
이윤환 <zoo7930@sju.ac.kr>
2022-06-25 13:59:58 +0900

    2022/6/26 - 숨바꼭질 3 재풀이

--- baekjoon/숨박꼭질 3.java
+++ baekjoon/숨박꼭질 3.java
@@ -3,33 +3,32 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-
         int answer = Integer.MAX_VALUE;
-        PriorityQueue<Node> queue = new PriorityQueue<>();
+        Queue<Node> queue = new ArrayDeque<>();
         int[] dp = new int[100001];
-        Arrays.fill(dp,-1);
+        Arrays.fill(dp,Integer.MAX_VALUE);
         queue.add(new Node(N, 0));
 
         while (!queue.isEmpty()) {
             Node current = queue.poll();
 
-            if (dp[current.position] < current.second) continue;
+            if (dp[current.position] <= current.second) continue;
+
+            dp[current.position] =  current.second;
 
             if(current.position == K) break;
 
-            int[] nextPosition = {current.position - 1, current.position + 1, current.position * 2};
-            int[] nextSecond = {current.second + 1, current.second + 1, current.second};
+            int[] nextPosition = {current.position * 2, current.position - 1, current.position + 1};
+            int[] nextSecond = { current.second, current.second + 1, current.second + 1};
             for (int i = 0; i < nextPosition.length; i++) {
                 if (nextPosition[i] >= 0 && nextPosition[i] <= 100000) {
-                    if (dp[nextPosition
-[i]] == -1) {
+                    if(dp[nextPosition[i]] > nextSecond[i]){
                         queue.add(new Node(nextPosition[i], nextSecond[i]));
                     }
                 }
@@ -37,19 +36,13 @@ public class Main {
         }
         System.out.println(dp[K]);
     }
-    static class Node implements Comparable<Node>{
+    static class Node{
         private int position;
         private int second;
 
         public Node(int position, int second) {
             this.position = position;
             this.second = second;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if(o.position == this.position) return o.second - this.second;
-            return o.position - this.position;
         }
     }
 }
