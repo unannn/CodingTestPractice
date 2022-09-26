d87f73e5e70ba80a6fb13353bfeadb354dde810a
LeeYunHwan <woooia1@gmail.com>
2022-09-27 00:00:11 +0900

    2022/9/27 - 크로아티아 알파벳 수정

--- baekjoon/크로아티아 알파벳.java
+++ baekjoon/크로아티아 알파벳.java
@@ -3,25 +3,30 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String[] alphabets = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String[] alphabets = {"dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="};
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[] s = br.readLine().toCharArray();
 
         int answer = s.length;
-        for (String alphabet : alphabets) {
-            char[] a = alphabet.toCharArray();
-            int temp = 0;
-            for(int i = 0; i < s.length - a.length + 1; i++){
-                int same = 0;
-                for(int j = 0; j < a.length; j++){
-                    same += a[j] == s[i+j] ? 1 : 0;
-                }
-                temp += same == a.length ? 1 : 0;
-            }
+        for (int i = 0, size = s.length; i < size; i++) {
+            for (String alphabet : alphabets) {
+                if (i + alphabet.length() - 1 < size) {
+                    char[] a = alphabet.toCharArray();
 
-            answer -= (a.length - 1)* temp;
+                    int j;
+                    for (j = 0; j < a.length; j++) {
+                        if (a[j] != s[i + j]) break;
+                    }
+
+                    if (j == a.length) {
+                        i += alphabet.length() - 1;
+                        answer -= alphabet.length() - 1;
+                        break;
+                    }
+                }
+            }
         }
         System.out.println(answer);
     }
